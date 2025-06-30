2ad32f10c9d4a4a9df23beefe14b456e33694baa
Manish Kumar <141416755+manishpcp@users.noreply.github.com>
2025-06-30 12:02:19 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-0a7d80731ae1b2435"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "assignment"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
